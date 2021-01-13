b05b799274c49f25e540bce92e62b55b95faa4b9
Evan C. Mascitti <ecm200@psu.edu>
2021-01-13 16:24:55 -0500

    added cylinder area as a data object

---
+++ R/cyl_cross_sect_area_cm2.R
@@ -0,0 +1,10 @@
+#'@title Cross-sectional area of one cylinder
+#'
+#'@description The pipe is 6 1/16" inner diameter. I often use the
+#'  cross-sectional area for calculations, so I am storing it here as a data
+#'  object in the **soilmesh** package.
+#'
+#'@format numeric vector of length 1'
+#'
+#'
+"cyl_cross_sect_area_cm2"

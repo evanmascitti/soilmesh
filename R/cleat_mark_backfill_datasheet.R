84b834bf229db45751d81b219a4cd9cf1a7a0ea5
Evan C. Mascitti <evanmascitti@gmail.com>
2021-04-12 16:57:00 -0400

    added additional columns for sand backfill datasheet

--- R/cleat_mark_backfill_datasheet.R
+++ R/cleat_mark_backfill_datasheet.R
@@ -14,7 +14,11 @@ cleat_mark_backfill_datasheet <- function(experiment_name, date, tin_tare_set,
     cylinder_ID = c("01", "02", "03", "04", "05", "06", "07", "08","09", "10", "11", "12"),
     sand_loose_density = sand_loose_density,
     sand_cup_mass_before_backfilling = "",
-    sand_cup_mass_after_backfilling = "")
+    sand_cup_mass_after_backfilling = "",
+    bowl_number = 1:12,
+    bowl_tare = "",
+    bowl_W_wet_sample = "",
+    bowl_w_OD_sample = "")
 
   file_path <- here::here("ecmdata", "raw-data", "cleat-mark-testing",
                           date, glue::glue("cleat-mark-backfill-data_{date}.csv"))
